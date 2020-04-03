e4527a8548a18d654881486e900e222ce4f80762
xOmenus <ddosfault@gmail.com>
2020-04-04 00:18:41 +0300

    change Exchange.class

--- src/main/java/Exchange.java
+++ src/main/java/Exchange.java
@@ -1,5 +1,23 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.w3c.dom.UserDataHandler;
+
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
 public class Exchange {
     public static void getValue() {
+        String[] array = new String[] {"USD", "AUD", "CAD", "PLN", "MXN"} ;
+        for (i=0;i<5;i++){
+            out.println("ВВедите пожалуйста интересующую вас валюту:\n" + array[i]);
+        }
 
+        String money;
+        HttpResponse<JsonNode> response = Unirest.get("http://data.fixer.io/api/latest")
+                .queryString("access_key", "88d05c9b618a117298e8f4870e2e3c95")
+                .queryString("symbols", money).asJson();
+        ret = response.getBody().getObject().getJSONObject("current").get(query).toString();
     }
 }
--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -10,7 +10,8 @@ import static java.lang.System.*;
 public class Weather {
 
     public static void getValue() throws UnirestException {
-        System.out.println("Введите пожалуйста интересующий город:");
+        String ret;
+        System.out.println("Введите пожалуйста интересующий город (на английском языке):");
         Scanner check = new Scanner(in);
         String gorod = check.nextLine();
         System.out.println("Выран город: " + gorod);
@@ -20,12 +21,19 @@ public class Weather {
                 "   -- observation_time (Время в GMT)");
         Scanner check1 = new Scanner(in);
         String query = check1.nextLine();
-        if (!query.contains("temperature") && !query.contains("observation_time")) {
+        if (query.contains("temperature") || query.contains("observation_time")) {
             System.out.println("Вырано поле: " + query);
             HttpResponse<JsonNode> response = Unirest.get("http://api.weatherstack.com/current")
                     .queryString("access_key", "d891f27929cb752cb66caeb1127e8b16")
                     .queryString("query", gorod).asJson();
-            out.println(response.getBody().getObject().getJSONObject("current").get(query));
+            ret = response.getBody().getObject().getJSONObject("current").get(query).toString();
+            if (query.contains("temperature")) {
+                out.println("Текущая температура в городе " + gorod + " " + ret + " градусов С");
+            }
+            else {
+                out.println("Текущее время в городе "+ gorod + ret + " по Гринвичу");
+            }
+
         } else out.println("Get Out Bustard!");
 
     }
